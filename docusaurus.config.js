f5f4522581d5713b7c9293c76d025b465934ced0
Admin <nikonikotrall@gmail.com>
2022-08-22 16:57:57 +0300

    Add link for page editing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,6 +43,7 @@ const config = {
                         }
                     },
                     sidebarPath: require.resolve('./sidebars.js'),
+                    editUrl: 'https://github.com/subsquid/docs/edit/master/'
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
