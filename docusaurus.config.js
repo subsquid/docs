6dc693da1fa16fe4cdafc132428a405311827bc6
Eugene Formanenko <mo4islona@gmail.com>
2023-02-01 15:00:06 +0400

    feat: add hotjar analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,120 +6,122 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Subsquid',
-    tagline: 'This documentation provides information for all degrees of expertise, varying from complete beginner, to those who only need a refresher on specific commands.',
-    url: 'https://docs.subsquid.io/',
-    baseUrl: '/',
-    onBrokenLinks: 'warn',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    staticDirectories: ['static'],
+  title: 'Subsquid',
+  tagline: 'This documentation provides information for all degrees of expertise, varying from complete beginner, to those who only need a refresher on specific commands.',
+  url: 'https://docs.subsquid.io/',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  staticDirectories: ['static'],
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'subsquid', // Usually your GitHub org/user name.
-    projectName: 'squid', // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'subsquid', // Usually your GitHub org/user name.
+  projectName: 'squid', // Usually your repo name.
 
-    // Even if you don't use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-    },
-    trailingSlash: true,
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  trailingSlash: true,
 
-    presets: [
-        [
-            'classic',
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: {
-                    routeBasePath: '/',
-                    lastVersion: 'current',
-                    versions: {
-                        current: {
-                            label: 'Firesquid',
-                            path: '/'
-                        }
-                    },
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl: 'https://github.com/subsquid/docs/edit/master/'
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-                gtag: {
-                    trackingID: 'G-WMH2V85G1B',
-                    anonymizeIP: true,
-                },
-            }),
-        ],
-
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: 'Firesquid',
+              path: '/'
+            }
+          },
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/subsquid/docs/edit/master/'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-WMH2V85G1B',
+          anonymizeIP: true,
+        },
+      }),
     ],
 
-    themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            navbar: {
-                logo: {
-                    alt: 'Subsquid',
-                    src: 'img/logo-light.svg',
-                    srcDark: 'img/logo-dark.svg',
-                },
-                items: [],
-                hideOnScroll: true
-            },
-            colorMode: {
-                defaultMode: 'light'
-            },
-            footer: {},
-            prism: {
-                additionalLanguages: ['docker', 'powershell'],
-                theme: lightCodeTheme,
-                darkTheme: {
-                    ...darkCodeTheme,
-                    plain: {
-                        color: "var(--fg-base--default)",
-                        backgroundColor: "var(--bg-base--indistinguishable)"
-                    }
-                },
-            },
-            algolia: {
-                // The application ID provided by Algolia
-                appId: '39MDW9KQHD',
+  ],
 
-                // Public API key: it is safe to commit it
-                apiKey: 'ab3ada75abec35a245193df3de026285',
+  themeConfig:  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */ ({
+    hotjar: {
+      applicationId: 3345082,
+    },
+    navbar: {
+      logo: {
+        alt: 'Subsquids',
+        src: 'img/logo-light.svg',
+        srcDark: 'img/logo-dark.svg',
+      },
+      items: [],
+      hideOnScroll: true
+    },
+    colorMode: {
+      defaultMode: 'light'
+    },
+    footer: {},
+    prism: {
+      additionalLanguages: ['docker', 'powershell'],
+      theme: lightCodeTheme,
+      darkTheme: {
+        ...darkCodeTheme,
+        plain: {
+          color: 'var(--fg-base--default)',
+          backgroundColor: 'var(--bg-base--indistinguishable)'
+        }
+      },
+    },
+    algolia: {
+      // The application ID provided by Algolia
+      appId: '39MDW9KQHD',
 
-                indexName: 'subsquid',
+      // Public API key: it is safe to commit it
+      apiKey: 'ab3ada75abec35a245193df3de026285',
 
-                // Optional: see doc section below
-                contextualSearch: true,
+      indexName: 'subsquid',
 
-                // Optional: Algolia search parameters
-                searchParameters: {},
+      // Optional: see doc section below
+      contextualSearch: true,
 
-                // Optional: path for search page that enabled by default (`false` to disable it)
-                searchPagePath: false,
+      // Optional: Algolia search parameters
+      searchParameters: {},
 
-                //... other Algolia params
-            },
-        }),
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: false,
 
-    plugins: [
-        async function tailWindCssPlugin(context, options) {
-            return {
-                name: 'docusaurus-tailwindcss',
-                configurePostCss(postcssOptions) {
-                    // Appends TailwindCSS and AutoPrefixer.
-                    postcssOptions.plugins.push(require("tailwindcss"));
-                    postcssOptions.plugins.push(require("autoprefixer"));
-                    return postcssOptions;
-                },
-            };
+      //... other Algolia params
+    },
+  }),
+
+  plugins: [
+    'docusaurus-plugin-hotjar',
+    async function tailWindCssPlugin(context, options) {
+      return {
+        name: 'docusaurus-tailwindcss',
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require('tailwindcss'));
+          postcssOptions.plugins.push(require('autoprefixer'));
+          return postcssOptions;
         },
-    ]
+      };
+    },
+  ]
 };
 
 module.exports = config;
