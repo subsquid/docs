8467f446353191826abed70d5fa9aa366748f8f2
Snegirev <sae.snegirev@gmail.com>
2023-02-16 06:54:11 +0500

    Redesign and rate the page widget
    Cosmetic edits according to the layout
    
    Cosmetic edits according to the layout
    
    Cosmetic edits according to the layout
    
    Cosmetic edits according to the layout
    
    Cosmetic edits according to the layout
    
    Cosmetic edits according to the layout
    
    Cosmetic edits according to the layout
    
    Cosmetic edits according to the layout
    
    Cosmetic edits according to the layout
    
    fix Analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,6 +50,9 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'G-WMH2V85G1B',
+        },
         gtag: {
           trackingID: 'G-WMH2V85G1B',
           anonymizeIP: true,
