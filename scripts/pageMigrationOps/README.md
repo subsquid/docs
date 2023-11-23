357b7104f37fb6fef674327b5593509143e84388
abernatskiy <abernatskiy@toha.li>
2023-11-23 22:55:16 +0900

    File existence checking script + cosmetics
    	modified:   correspondences.log
    	new file:   scripts/pageMigrationOps/README.md
    	new file:   scripts/pageMigrationOps/checkFilesExistence.sh

---
+++ scripts/pageMigrationOps/README.md
@@ -0,0 +1,7 @@
+A collection of scripts for handling page migrations using path correspondences files, that is, movement logs in the following format:
+```
+deploy-squid/deploy-manifest.mdx -> cloud/reference/manifest.mdx
+deploy-squid/self-hosting.md -> sdk/resources/self-hosting.md
+deploy-squid/troubleshooting.md -> cloud/troubleshooting.md
+deploy-squid -> cloud
+```
---
+++ scripts/pageMigrationOps/checkFilesExistence.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usage: checkFileExistence.sh <path_to_log> <log_column>
+# E.g. to check if all sorce files in correspondences.log exist, use
+#   $ checkFileExistence.sh correspondences.log 1
+# To check if destination files exist, use
+#   $ checkFileExistence.sh correspondences.log 3
+
+IFS=$'\n'
+for path in `cut -d ' ' -f $2 $1`; do
+	if [[ "$path" =~ .*"md"|"mdx"$ ]]; then
+		if [ ! -f "$path" ]; then
+			echo File $path not found or is not a regular file
+		fi
+	else
+		if [ ! -d "$path" ]; then
+			echo Directory $path not found or is not a directory
+		fi
+	fi
+done
